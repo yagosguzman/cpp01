60c0907d20e4781828a7081f163536a1c03244bc
Yago <129056609+yagosguzman@users.noreply.github.com>
2024-08-06 14:36:44 +0000

    ex02 done, wip ex03

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,14 +5,14 @@
 int main(void){
     std::string string = "HI THIS IS BRAIN";
     std::string *stringPTR = &string;
-    std::string stringREF = string;
+    std::string &stringREF = string;
 
     std::cout << "Address of string: " << &string << std::endl;
     std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
-    std::cout << "Address of stringREF: " << stringREF << std::endl; // Incorrecto
+    std::cout << "Address of stringREF: " << &stringREF << std::endl;
 
     std::cout << "Value of string: " << string << std::endl;
-    std::cout << "Value of stringPTR: " << stringPTR << std::endl;
-    std::cout << "Value of stringREF: " << stringREF << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl; // el value al que apunta o la direccion almacenada??
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
     return (0);
 }
